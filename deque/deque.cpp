62f34000b6b07b45d08c21081bbe855ae7e31470
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-05-29 02:47:10 +0500

    DEQUE

---
+++ deque/deque.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<String>
+#include<deque>
+using namespace std;
+
+int main(){
+	deque<string> stn = {"Taha","Rajput","CRD"};
+	for(string k: stn){
+		cout << k << "\n";
+		
+	}
+	return 0;
+	
+}
