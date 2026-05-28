bdd3fe5e94cd32684a79d76b19d75178641d5cc3
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-05-29 02:48:57 +0500

    vector

---
+++ vector/vector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<ctime>
+#include<cmath>
+
+using namespace std;
+int main(){
+	vector<string> car;
+	car.push_back("Civics");
+	//cout << car[0];
+	car.clear();
+	cout << car.size();
+	return 0;
+}
